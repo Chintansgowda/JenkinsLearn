e9df5a42a7be6e5e46ab385d9129fe54ca7085ef
Chintan_06 <159480965+Chintansgowda@users.noreply.github.com>
2026-04-07 13:50:24 +0530

    Fix shebang and update echo messages in freestyle.sh

--- freestyle/freestyle.sh
+++ freestyle/freestyle.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-echo " file for just jenkins practice "
+echo " file for just jenkins practice - freestyle "
 
 echo " Configured a Job with poll scm - hence lets make changes to this git repo and lets verfiy poll scm "
